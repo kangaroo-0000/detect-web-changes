8c48d9bff61485aeac13e120a0879b6b3ce5202e
Beckham Pan <beckham.0831@yahoo.com.tw>
2022-08-11 11:48:20 +0800

    finalize the logic

--- test.py
+++
@@ -1,9 +0,0 @@
-import main
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.support.select import By
-from selenium.webdriver.support.ui import WebDriverWait
-
-new = main.BrowserContent(browser=webdriver.Chrome(service=Service(ChromeDriverManager().install())), url='https://www.dragonsteel.com.tw/')
-print(new.list_ids())
